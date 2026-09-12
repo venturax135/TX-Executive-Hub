e6ef3b5322c730404ca54ea94a3a4ca6393fe368
venturax135 <77841271+venturax135@users.noreply.github.com>
2026-09-12 14:10:31 -0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "txhub-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/js/prompts.js"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+
+  );
+
+});
